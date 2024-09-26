84da04f737321171c180d0510471ff73ec79bb1d
LostBoy143 <shubhamsinghbihar007@gmail.com>
2024-09-26 22:47:11 +0530

    learnt about props

---
+++ src/module1.mjs
@@ -0,0 +1,6 @@
+const a = "Shubham Singh";
+const b = "Sahil Singh";
+const c = "surya dada";
+
+export default a;
+export { b, c };
---
+++ src/module2.mjs
@@ -0,0 +1,5 @@
+import myname from "./module1.mjs";
+import { b, c } from "./module1.mjs";
+
+console.log(c);
+console.log(b);
