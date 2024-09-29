1f935aa81e6dba23d5e33e529ae8a018f83b93b8
LostBoy143 <shubhamsinghbihar007@gmail.com>
2024-09-29 22:49:08 +0530

    modification

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import About from "./Components/About";
 import Navbar from "./Components/Navbar";
 
 import TextForm from "./Components/TextForm";
@@ -13,7 +12,6 @@ function App() {
       />
       <div className="container my-3 mt-5">
         <TextForm heading="Enter the text to analyze" />
-        <About />
       </div>
     </>
   );
